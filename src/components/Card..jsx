dda9db969b3bae87a1a7e2890493bd9b71ff69c1
severo <oliveira.severo@gmail.com>
2021-11-12 09:45:40 -0300

    Declarando e validando o tipo das props em card.jsx

---
+++ src/components/Card..jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    return (
+      <p>jhhyuuyuyu</p>
+    );
+  }
+}
+export default Card;

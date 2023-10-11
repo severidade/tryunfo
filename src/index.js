3b575c3b9a2c17a04a6501f637ec8c95701030cd
severo <oliveira.severo@gmail.com>
2023-10-11 09:00:35 -0300

    feat: :rocket: Rastreio da Página inicial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA4 from 'react-ga4'; 
 import './index.css';
 import App from './App';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './stylesMaterialUi/Theme';
+
+// ReactGA4.initialize('G-RZ8L31ERFQ');
+
+const TRACKING_ID = 'G-RZ8L31ERFQ'
+ 
+useEffect(() => {
+  ReactGA4.initialize(TRACKING_ID); // Inicialize o React-GA4
+  ReactGA4.pageview(window.location.pathname); // Rastreie a visualização da página inicial
+}, []);
+
 
 ReactDOM.render(
   <React.StrictMode>
